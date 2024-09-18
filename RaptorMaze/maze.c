b88c43c3a16dcc390bd484c4b8b348b572cc565a
stuckia <abigailstucki@outlook.com>
2024-09-17 19:39:14 -0600

    Added Makefile

--- RaptorMaze/maze.c
+++ RaptorMaze/maze.c
@@ -1,1 +1,24 @@
-// TESTING
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+
+//module metadata
+MODULE_AUTHOR("Abby Stucki");
+MODULE_DESCRIPTION("Raptor Maze generator");
+MODULE_LICENSE("GPL");
+
+//init method
+static int __init maze_init(void) {
+	printk(KERN_INFO "Raptor Maze generating...");
+	return 0;
+}
+
+//exit method
+static void __exit maze_exit(void) {
+	printk(KERN_INFO "Raptor Maze closing...");
+}
+
+module_init(maze_init);
+module_exit(maze_exit);
